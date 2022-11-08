095eb1a413a74870f844261af1ee5ba759a1bf70
LinhBQ <linhstartwork@gmail.com>
2022-11-08 11:11:25 +0700

    Update messages response

---
+++ app/api/utils/messages.py
@@ -0,0 +1,9 @@
+class RESPONSE_ERRORS:
+    DEFAULT = 'Error'
+    USER_NOT_FOUND = 'User is not founded'
+    WRONG_PW = 'Password is not correct'
+    INVALID_DATA = 'Data is invalid'
+
+
+class RESPONSE_OK:
+    DEFAULT = 'OK'
